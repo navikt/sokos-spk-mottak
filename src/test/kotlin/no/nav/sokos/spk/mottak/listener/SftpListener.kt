a7e87cf8336de0cbef18bf88cc5dd093d8dc68be
takwaiwang <tak.wai.wang@nav.no>
2024-03-27 12:39:51 +0100

    Tar i bruk kotest listener

---
+++ src/test/kotlin/no/nav/sokos/spk/mottak/listener/SftpListener.kt
@@ -0,0 +1,94 @@
+package no.nav.sokos.spk.mottak.listener
+
+import com.github.dockerjava.api.model.ExposedPort
+import com.github.dockerjava.api.model.PortBinding
+import com.github.dockerjava.api.model.Ports
+import io.kotest.core.listeners.AfterSpecListener
+import io.kotest.core.listeners.BeforeSpecListener
+import io.kotest.core.spec.Spec
+import no.nav.sokos.spk.mottak.config.PropertiesConfig
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.images.builder.Transferable
+import org.testcontainers.shaded.org.bouncycastle.crypto.AsymmetricCipherKeyPair
+import org.testcontainers.shaded.org.bouncycastle.crypto.generators.Ed25519KeyPairGenerator
+import org.testcontainers.shaded.org.bouncycastle.crypto.params.AsymmetricKeyParameter
+import org.testcontainers.shaded.org.bouncycastle.crypto.params.Ed25519KeyGenerationParameters
+import org.testcontainers.shaded.org.bouncycastle.crypto.util.OpenSSHPrivateKeyUtil
+import org.testcontainers.shaded.org.bouncycastle.crypto.util.OpenSSHPublicKeyUtil
+import org.testcontainers.shaded.org.bouncycastle.util.io.pem.PemObject
+import org.testcontainers.shaded.org.bouncycastle.util.io.pem.PemWriter
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.OutputStreamWriter
+import java.nio.charset.StandardCharsets
+import java.security.SecureRandom
+import java.util.Base64
+
+object SftpListener : BeforeSpecListener, AfterSpecListener {
+    private val keyPair = generateKeyPair()
+    private val privateKeyFile = createPrivateKeyFile(keyPair.private)
+    private val genericContainer = setupSftpTestContainer(keyPair.public)
+
+    val sftpConfig = PropertiesConfig.SftpConfig(
+        host = "localhost",
+        username = "foo",
+        privateKey = privateKeyFile.absolutePath,
+        privateKeyPassword = "pass",
+        port = 5678
+    )
+
+    override suspend fun beforeSpec(spec: Spec) {
+        genericContainer.start()
+
+    }
+
+    override suspend fun afterSpec(spec: Spec) {
+        genericContainer.stop()
+    }
+
+    private fun setupSftpTestContainer(publicKey: AsymmetricKeyParameter): GenericContainer<*> {
+        val publicKeyAsBytes = convertToByteArray(publicKey)
+        return GenericContainer("atmoz/sftp:alpine")
+            .withCopyToContainer(
+                Transferable.of(publicKeyAsBytes),
+                "/home/foo/.ssh/keys/id_rsa.pub"
+            )
+            .withExposedPorts(22)
+            .withCreateContainerCmdModifier { cmd -> cmd.hostConfig!!.withPortBindings(PortBinding(Ports.Binding.bindPort(5678), ExposedPort(22))) }
+            .withCommand("foo::::inbound,inbound/ferdig,outbound")
+    }
+
+    private fun createPrivateKeyFile(privateKey: AsymmetricKeyParameter): File {
+        val privateKeyString = convertToString(privateKey)
+        return File("src/test/resources/privateKey").apply {
+            writeText(privateKeyString)
+        }
+    }
+
+    private fun generateKeyPair(): AsymmetricCipherKeyPair {
+        val keyPairGenerator = Ed25519KeyPairGenerator()
+        keyPairGenerator.init(Ed25519KeyGenerationParameters(SecureRandom()))
+        return keyPairGenerator.generateKeyPair()
+    }
+
+    private fun convertToString(privateKey: AsymmetricKeyParameter): String {
+        val outputStream = ByteArrayOutputStream()
+        PemWriter(OutputStreamWriter(outputStream)).use { writer ->
+            val encodedPrivateKey =
+                OpenSSHPrivateKeyUtil.encodePrivateKey(privateKey)
+            writer.writeObject(
+                PemObject(
+                    "OPENSSH PRIVATE KEY",
+                    encodedPrivateKey
+                )
+            )
+        }
+        return outputStream.toString()
+    }
+
+    private fun convertToByteArray(publicKey: AsymmetricKeyParameter): ByteArray {
+        val openSshEncodedPublicKey = OpenSSHPublicKeyUtil.encodePublicKey(publicKey)
+        val base64EncodedPublicKey = Base64.getEncoder().encodeToString(openSshEncodedPublicKey)
+        return "ssh-ed25519 $base64EncodedPublicKey".toByteArray(StandardCharsets.UTF_8)
+    }
+}
--- src/test/kotlin/no/nav/sokos/spk/mottak/service/FtpServiceTest.kt
+++ src/test/kotlin/no/nav/sokos/spk/mottak/service/FtpServiceTest.kt
@@ -1,50 +1,19 @@
 package no.nav.sokos.spk.mottak.service
 
-import com.github.dockerjava.api.model.ExposedPort
-import com.github.dockerjava.api.model.PortBinding
-import com.github.dockerjava.api.model.Ports
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import java.io.ByteArrayOutputStream
-import java.io.File
-import java.io.OutputStreamWriter
-import java.nio.charset.StandardCharsets
-import java.security.SecureRandom
-import java.util.Base64.getEncoder
-import no.nav.sokos.spk.mottak.config.PropertiesConfig
 import no.nav.sokos.spk.mottak.config.SftpConfig
-import org.testcontainers.containers.GenericContainer
-import org.testcontainers.images.builder.Transferable
-import org.testcontainers.shaded.org.bouncycastle.crypto.AsymmetricCipherKeyPair
-import org.testcontainers.shaded.org.bouncycastle.crypto.generators.Ed25519KeyPairGenerator
-import org.testcontainers.shaded.org.bouncycastle.crypto.params.AsymmetricKeyParameter
-import org.testcontainers.shaded.org.bouncycastle.crypto.params.Ed25519KeyGenerationParameters
-import org.testcontainers.shaded.org.bouncycastle.crypto.util.OpenSSHPrivateKeyUtil
-import org.testcontainers.shaded.org.bouncycastle.crypto.util.OpenSSHPublicKeyUtil
-import org.testcontainers.shaded.org.bouncycastle.util.io.pem.PemObject
-import org.testcontainers.shaded.org.bouncycastle.util.io.pem.PemWriter
+import no.nav.sokos.spk.mottak.listener.SftpListener
 
 
 internal class FtpServiceTest : FunSpec({
+    extensions(SftpListener)
 
-    val keyPair = generateKeyPair()
-    val privateKeyFile = createPrivateKeyFile(keyPair.private)
-    val genericContainer = setupSftpTestContainer(keyPair.public)
-    lateinit var ftpService: FtpService
-
-
-    beforeSpec {
-        genericContainer.start()
-        val sftpSession = SftpConfig(sftpConfig(privateKeyFile)).createSftpConnection()
-        ftpService = FtpService(sftpSession)
-    }
-
-    afterSpec {
-        genericContainer.stop()
+    val ftpService: FtpService by lazy {
+        FtpService(SftpConfig(SftpListener.sftpConfig).createSftpConnection())
     }
 
     test("test oppretting av fil i inbound og hente den ut") {
-
         ftpService.createFile("test.txt", Directories.INBOUND, "content")
 
         val downloadedFiles = ftpService.downloadFiles()
@@ -53,7 +22,6 @@ internal class FtpServiceTest : FunSpec({
     }
 
     test("test flytting av fil fra inbound til outbound") {
-
         ftpService.createFile("test.txt", Directories.INBOUND, "content")
 
         val downloadedFiles = ftpService.downloadFiles()
@@ -68,59 +36,4 @@ internal class FtpServiceTest : FunSpec({
         downloadedFilesFromOutbound.size shouldBe 1
 
     }
-
 })
-
-private fun sftpConfig(privateKeyFile: File) = PropertiesConfig.SftpConfig(
-    host = "localhost",
-    username = "foo",
-    privateKey = privateKeyFile.absolutePath,
-    privateKeyPassword = "pass",
-    port = 5678
-)
-
-private fun setupSftpTestContainer(publicKey: AsymmetricKeyParameter): GenericContainer<*> {
-    val publicKeyAsBytes = convertToByteArray(publicKey)
-    return GenericContainer("atmoz/sftp:alpine")
-        .withCopyToContainer(
-            Transferable.of(publicKeyAsBytes),
-            "/home/foo/.ssh/keys/id_rsa.pub"
-        )
-        .withExposedPorts(22)
-        .withCreateContainerCmdModifier { cmd -> cmd.hostConfig!!.withPortBindings(PortBinding(Ports.Binding.bindPort(5678), ExposedPort(22))) }
-        .withCommand("foo::::inbound,inbound/ferdig,outbound")
-}
-
-private fun createPrivateKeyFile(privateKey: AsymmetricKeyParameter): File {
-    val privateKeyString = convertToString(privateKey)
-    return File("src/test/resources/privateKey").apply {
-        writeText(privateKeyString)
-    }
-}
-
-private fun generateKeyPair(): AsymmetricCipherKeyPair {
-    val keyPairGenerator = Ed25519KeyPairGenerator()
-    keyPairGenerator.init(Ed25519KeyGenerationParameters(SecureRandom()))
-    return keyPairGenerator.generateKeyPair()
-}
-
-private fun convertToString(privateKey: AsymmetricKeyParameter): String {
-    val outputStream = ByteArrayOutputStream()
-    PemWriter(OutputStreamWriter(outputStream)).use { writer ->
-        val encodedPrivateKey =
-            OpenSSHPrivateKeyUtil.encodePrivateKey(privateKey)
-        writer.writeObject(
-            PemObject(
-                "OPENSSH PRIVATE KEY",
-                encodedPrivateKey
-            )
-        )
-    }
-    return outputStream.toString()
-}
-
-private fun convertToByteArray(publicKey: AsymmetricKeyParameter): ByteArray {
-    val openSshEncodedPublicKey = OpenSSHPublicKeyUtil.encodePublicKey(publicKey)
-    val base64EncodedPublicKey = getEncoder().encodeToString(openSshEncodedPublicKey)
-    return "ssh-ed25519 $base64EncodedPublicKey".toByteArray(StandardCharsets.UTF_8)
-}
