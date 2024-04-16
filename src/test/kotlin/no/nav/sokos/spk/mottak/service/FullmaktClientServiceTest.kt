1e2a5a7ebd8df060a48a27fb3b0bb64973a3cfb4
jonasnico <jonas.nicolaysen@nav.no>
2024-04-16 13:03:33 +0200

    :white_check_mark: test for å hente fullmakter

--- src/test/kotlin/no/nav/sokos/spk/mottak/service/FullmaktClientServiceTest.kt
+++ src/test/kotlin/no/nav/sokos/spk/mottak/service/FullmaktClientServiceTest.kt
@@ -1,7 +1,9 @@
 package no.nav.sokos.spk.mottak.service
 
 import com.github.tomakehurst.wiremock.WireMockServer
-import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.client.WireMock.get
+import com.github.tomakehurst.wiremock.client.WireMock.okJson
+import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.extensions.wiremock.ListenerMode
 import io.kotest.extensions.wiremock.WireMockListener
@@ -12,38 +14,42 @@ import no.nav.sokos.spk.mottak.TestHelper.readFromResource
 import no.nav.sokos.spk.mottak.integration.FullmaktClientService
 import no.nav.sokos.spk.mottak.security.AccessTokenClient
 
+val accessToken = mockk<AccessTokenClient>()
+
 class FullmaktClientServiceTest : FunSpec({
+
     val fullmaktClientServiceServer = WireMockServer(9000)
     fullmaktClientServiceServer.start()
     listener(WireMockListener(fullmaktClientServiceServer, ListenerMode.PER_SPEC))
-    val accessToken = mockk<AccessTokenClient>()
     val fullmaktClientService = FullmaktClientService(pensjonFullmaktUrl = fullmaktClientServiceServer.baseUrl(), accessTokenClient = accessToken)
 
     afterSpec {
         fullmaktClientServiceServer.stop()
     }
 
-    test("skal returnere riktig antall fullmakter") {
+    test("skal returnere fullmakter") {
 
         fullmaktClientServiceServer.stubFor(
-            WireMock.get(WireMock.urlEqualTo("/finnFullmaktMottakere?side=0&antall=1000&koderFullmaktType=PENGEMOT%2CVERGE_PENGEMOT"))
-                //.withQueryParam("side", equalTo("1"))
-                //.withQueryParam("antall", WireMock.equalTo("1000"))
-                //.withQueryParam("koderFullmaktType", WireMock.equalTo("PENGEMOT,VERGE_PENGEMOT"))
-                .willReturn(WireMock.okJson("fullmakter.json".readFromResource()))
+            get(urlEqualTo("/finnFullmaktMottakere?side=0&antall=1000&koderFullmaktType=PENGEMOT%2CVERGE_PENGEMOT"))
+                .willReturn(okJson("fullmakter.json".readFromResource()))
         )
-
         fullmaktClientServiceServer.stubFor(
-            WireMock.get(WireMock.urlEqualTo("/finnFullmaktMottakere?side=1&antall=1000&koderFullmaktType=PENGEMOT%2CVERGE_PENGEMOT"))
-                //.withQueryParam("side", equalTo("1"))
-                //.withQueryParam("antall", WireMock.equalTo("1000"))
-                //.withQueryParam("koderFullmaktType", WireMock.equalTo("PENGEMOT,VERGE_PENGEMOT"))
-                .willReturn(WireMock.okJson("[]"))
+            get(urlEqualTo("/finnFullmaktMottakere?side=1&antall=1000&koderFullmaktType=PENGEMOT%2CVERGE_PENGEMOT"))
+                .willReturn(okJson("[]"))
         )
-
         coEvery { accessToken.hentAccessToken() } returns "token"
-        val actualFullmakter = fullmaktClientService.hentAlleFullmakter()
 
-        actualFullmakter.size shouldBe 95
+        val actualFullmakter = fullmaktClientService.hentAlleFullmakter()
+        val expectedResult = mapOf(
+            "11528524674" to "10488337381",
+            "04127604695" to "11058114091",
+            "61128149685" to "07028229873",
+            "19917199240" to "03085623833",
+            "10410351752" to "11515509803"
+        )
+
+        actualFullmakter.size shouldBe 5
+        actualFullmakter shouldBe expectedResult
     }
+
 })
