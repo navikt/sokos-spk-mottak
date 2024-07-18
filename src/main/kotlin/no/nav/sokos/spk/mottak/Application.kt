44003a65b564d7c1b75d8ac0572dd4c3afe0c1e3
takwaiwang <tak.wai.wang@nav.no>
2024-07-18 15:17:15 +0200

    Fiks flyway migration

--- src/main/kotlin/no/nav/sokos/spk/mottak/Application.kt
+++ src/main/kotlin/no/nav/sokos/spk/mottak/Application.kt
@@ -3,14 +3,18 @@ package no.nav.sokos.spk.mottak
 import io.ktor.server.application.Application
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import mu.KotlinLogging
 import no.nav.sokos.spk.mottak.config.ApplicationState
-import no.nav.sokos.spk.mottak.config.DatabaseConfig
+import no.nav.sokos.spk.mottak.config.DatabaseConfig.postgresDataSource
 import no.nav.sokos.spk.mottak.config.JobTaskConfig
 import no.nav.sokos.spk.mottak.config.PropertiesConfig
 import no.nav.sokos.spk.mottak.config.applicationLifecycleConfig
 import no.nav.sokos.spk.mottak.config.commonConfig
 import no.nav.sokos.spk.mottak.config.routingConfig
 import no.nav.sokos.spk.mottak.config.securityConfig
+import org.flywaydb.core.Flyway
+
+private val logger = KotlinLogging.logger {}
 
 fun main() {
     embeddedServer(Netty, port = 8080, module = Application::module).start(true)
@@ -24,7 +28,13 @@ private fun Application.module() {
     securityConfig(useAuthentication)
     routingConfig(useAuthentication, applicationState)
 
-    DatabaseConfig.postgresMigrate()
+    Flyway
+        .configure()
+        .dataSource(postgresDataSource(role = PropertiesConfig.PostgresProperties().adminUser))
+        .load()
+        .migrate()
+    logger.info { "Migration finished" }
+
     // JmsConsumerListenerService()
     JobTaskConfig.scheduler().start()
 }
