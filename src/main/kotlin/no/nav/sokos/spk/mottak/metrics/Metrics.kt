119c50e3368e5f683c8b4c29e113bcf965fd1f55
takwaiwang <tak.wai.wang@nav.no>
2024-07-18 14:31:19 +0200

    endre flyway script for db-scheduler

--- src/main/kotlin/no/nav/sokos/spk/mottak/metrics/Metrics.kt
+++ src/main/kotlin/no/nav/sokos/spk/mottak/metrics/Metrics.kt
@@ -10,56 +10,64 @@ object Metrics {
     val prometheusMeterRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
 
     val countTrekkTransaksjonerTilOppdrag: Counter =
-        Counter.builder()
+        Counter
+            .builder()
             .name("${METRICS_NAMESPACE}_trekkTransaksjonerTilOppdrag")
             .help("Counts the number of trekk transactions sent to OppdragZ")
             .withoutExemplars()
             .register(prometheusMeterRegistry.prometheusRegistry)
 
     val countUtbetalingTransaksjonerTilOppdrag: Counter =
-        Counter.builder()
+        Counter
+            .builder()
             .name("${METRICS_NAMESPACE}_utbetalingTransaksjonerTilOppdrag")
             .help("Counts the number of transactions sent to OppdragZ")
             .withoutExemplars()
             .register(prometheusMeterRegistry.prometheusRegistry)
 
     val countTransaksjonGodkjent: Counter =
-        Counter.builder()
+        Counter
+            .builder()
             .name("${METRICS_NAMESPACE}_transaksjonGodkjent")
             .help("Counts the number of file processed from SPK")
             .withoutExemplars()
             .register(prometheusMeterRegistry.prometheusRegistry)
 
     val countTransaksjonAvvist: Counter =
-        Counter.builder()
+        Counter
+            .builder()
             .name("${METRICS_NAMESPACE}_transaksjonAvvist")
             .help("Counts the number of file processed from SPK")
             .withoutExemplars()
             .register(prometheusMeterRegistry.prometheusRegistry)
 
     val countFileProcessed: Counter =
-        Counter.builder()
+        Counter
+            .builder()
             .name("${METRICS_NAMESPACE}_fileProcessed")
             .help("Counts the number of file processed from SPK")
             .withoutExemplars()
             .register(prometheusMeterRegistry.prometheusRegistry)
 
     val countInnTransaksjon: Counter =
-        Counter.builder()
+        Counter
+            .builder()
             .name("${METRICS_NAMESPACE}_innTransaksjoner")
             .help("Counts the number of transactions received from SPK")
             .withoutExemplars()
             .register(prometheusMeterRegistry.prometheusRegistry)
 
     val mqProducerMetricCounter: Counter =
-        Counter.builder()
+        Counter
+            .builder()
             .name("${METRICS_NAMESPACE}_mq_producer")
             .help("Counts the number of messages sent to OppdragZ via MQ")
             .withoutExemplars()
             .register(prometheusMeterRegistry.prometheusRegistry)
 
     val mqConsumerMetricCounter: Counter =
-        Counter.builder()
+        Counter
+            .builder()
             .name("${METRICS_NAMESPACE}_mq_consumer")
             .help("Counts the number of messages receive from OppdragZ via MQ")
             .withoutExemplars()
