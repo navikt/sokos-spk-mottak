ceab5c3a8e10934e2fd1795b18049cc9704a6f99
mudasar187 <mudasar_cheema@hotmail.com>
2024-07-18 19:39:35 +0200

    tester med flyway 10.15.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -101,6 +101,7 @@ dependencies {
     implementation("com.ibm.db2:jcc:$db2JccVersion")
     implementation("org.postgresql:postgresql:$postgresVersion")
     implementation("org.flywaydb:flyway-core:$flywayVersion")
+    runtimeOnly("org.flywaydb:flyway-database-postgresql:$flywayVersion")
     implementation("com.github.seratch:kotliquery:$kotliqueryVersion")
 
     // Config
