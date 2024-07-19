0a896fda393994ac70639b34a0200281376e1613
takwaiwang <tak.wai.wang@nav.no>
2024-07-19 09:01:21 +0200

    deploy med flyway-database-postgresql og versjon 10.15.2

--- src/main/kotlin/no/nav/sokos/spk/mottak/config/DatabaseConfig.kt
+++ src/main/kotlin/no/nav/sokos/spk/mottak/config/DatabaseConfig.kt
@@ -81,6 +81,7 @@ object DatabaseConfig {
             .dataSource(dataSource)
             .initSql("""SET ROLE "${PropertiesConfig.PostgresProperties().adminUser}"""")
             .lockRetryCount(-1)
+            .validateMigrationNaming(true)
             .load()
             .migrate()
             .migrationsExecuted
