6f65bb73fab1721fa44b9e6d3e108300de154d92
mudasar187 <mudasar_cheema@hotmail.com>
2024-07-23 15:08:43 +0200

    endre metrikknavn

--- src/main/kotlin/no/nav/sokos/spk/mottak/metrics/Metrics.kt
+++ src/main/kotlin/no/nav/sokos/spk/mottak/metrics/Metrics.kt
@@ -4,14 +4,12 @@ import io.micrometer.core.instrument.Timer
 import io.micrometer.prometheusmetrics.PrometheusConfig
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 import io.prometheus.metrics.core.metrics.Counter
-import java.util.concurrent.ConcurrentHashMap
 
 private const val METRICS_NAMESPACE = "sokos_spk_mottak"
 const val DATABASE_CALL = "database_call"
 const val SERVICE_CALL = "service_call"
 
 object Metrics {
-    private val counterCache = ConcurrentHashMap<String, Counter>()
     val prometheusMeterRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
 
     val timer: (metricName: String, className: String, method: String) -> Timer = { metricName, className, method ->
@@ -58,7 +56,7 @@ object Metrics {
     val fileProcessedCounter: Counter =
         Counter
             .builder()
-            .name("${METRICS_NAMESPACE}_file_processed")
+            .name("${METRICS_NAMESPACE}_innlest_fil")
             .help("Counts the number of file processed from SPK")
             .withoutExemplars()
             .register(prometheusMeterRegistry.prometheusRegistry)
