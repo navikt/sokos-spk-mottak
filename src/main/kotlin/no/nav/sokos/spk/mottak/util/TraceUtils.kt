5ee50a82e0d4813a83331f3803afe04e18b4b4db
takwaiwang <tak.wai.wang@nav.no>
2025-05-26 08:03:34 +0200

    test without MDC for openTelemetry

--- src/main/kotlin/no/nav/sokos/spk/mottak/util/TraceUtils.kt
+++ src/main/kotlin/no/nav/sokos/spk/mottak/util/TraceUtils.kt
@@ -2,25 +2,24 @@ package no.nav.sokos.spk.mottak.util
 
 import io.opentelemetry.api.GlobalOpenTelemetry
 import io.opentelemetry.api.trace.Tracer
-import org.slf4j.MDC
 
 object TraceUtils {
     val openTelemetry = GlobalOpenTelemetry.get()
 
     fun <T> withTracerId(
-        tracer: Tracer = openTelemetry.getTracer("no.nav.sokos.spk.mottak"),
+        tracer: Tracer = openTelemetry.getTracer(this::class.java.canonicalName),
         spanName: String = "withTracerId",
         block: () -> T,
     ): T {
         val span = tracer.spanBuilder(spanName).startSpan()
         val context = span.spanContext
-        MDC.put("trace_id", context.traceId)
-        MDC.put("span_id", context.spanId)
+//        MDC.put(LoggingContextConstants.TRACE_ID, context.traceId)
+//        MDC.put(LoggingContextConstants.SPAN_ID, context.spanId)
         return try {
             block()
         } finally {
-            MDC.remove("trace_id")
-            MDC.remove("span_id")
+//            MDC.remove(LoggingContextConstants.TRACE_ID)
+//            MDC.remove(LoggingContextConstants.SPAN_ID)
             span.end()
         }
     }
