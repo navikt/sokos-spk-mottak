5e3efa4e2d8f1e7e92d2e36a6becf091a5d93f41
takwaiwang <tak.wai.wang@nav.no>
2025-08-18 14:08:53 +0200

    Revert gradleup shadow version

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     kotlin("jvm") version "2.2.10"
     kotlin("plugin.serialization") version "2.2.10"
     id("com.expediagroup.graphql") version "8.8.1"
-    id("com.gradleup.shadow") version "9.0.2"
+    id("com.gradleup.shadow") version "9.0.0"
     id("org.jlleitschuh.gradle.ktlint") version "13.0.0"
     id("org.jetbrains.kotlinx.kover") version "0.9.1"
 }
