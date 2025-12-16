5556618f94e2412d1ab6bfcbab8f1cfe863a3fa6
mudasar187 <mudasar_cheema@hotmail.com>
2025-12-16 11:16:08 +0100

    fjerne resolutionstrategy for ktlint pga. versionen er fikset

--- build.gradle.kts
+++ build.gradle.kts
@@ -148,11 +148,6 @@ dependencies {
     testImplementation("org.wiremock:wiremock:$wiremockVersion")
 }
 
-// Vulnerability fix because of id("org.jlleitschuh.gradle.ktlint") uses ch.qos.logback:logback-classic:1.3.5
-configurations.ktlint {
-    resolutionStrategy.force("ch.qos.logback:logback-classic:$logbackVersion")
-}
-
 application {
     mainClass.set("no.nav.sokos.spk.mottak.ApplicationKt")
 }
