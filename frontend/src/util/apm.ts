58ae446adcf14fafe58b0a70190349eabc828e0d
mudasar187 <mudasar.cheema@nav.no>
2026-09-26 11:04:14 +0200

    chore: fikse apm filtering av url etc

--- frontend/src/util/apm.ts
+++ frontend/src/util/apm.ts
@@ -13,5 +13,17 @@ export function initApm() {
 		app: getAppName(),
 		tracing: true,
 		devConsoleEcho: false,
+		beforeSend: (item) => {
+			if (item.meta?.page?.url) {
+				try {
+					const url = new URL(item.meta.page.url);
+					url.search = "";
+					item.meta.page.url = url.toString();
+				} catch {
+					return item;
+				}
+			}
+			return item;
+		},
 	});
 }
